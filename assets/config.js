3d0f5229dad9c4f1718f0e448d53611a26614cb7
tantantzy <reyesneslyn2@gmail.com>
2026-07-26 15:33:52 +0800

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,8 @@
+/*
+Paste your real values from Supabase Project Settings → API.
+
+Use the publishable key or legacy anon key.
+Never use the service_role key here.
+*/
+export const SUPABASE_URL = "https://nbfmofeosorlvgapszzh.supabase.co";
+export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_t1_IuYiEj03rXGS-EwM23w_AXd0CS4L";
